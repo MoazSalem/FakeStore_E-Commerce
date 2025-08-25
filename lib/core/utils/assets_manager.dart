b0c8eb83563b67e69d71b89dea50b419393c69ba
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-08-25 14:30:32 +0300

    Custom assets and svgs

---
+++ lib/core/utils/assets_manager.dart
@@ -0,0 +1,15 @@
+class AssetsManager {
+  static const String home = 'assets/images/svgs/home.svg.vec';
+  static const String homeFilled = 'assets/images/svgs/home_filled.svg.vec';
+  static const String cart = 'assets/images/svgs/cart.svg.vec';
+  static const String cartFilled = 'assets/images/svgs/cart_filled.svg.vec';
+  static const String heart = 'assets/images/svgs/heart.svg.vec';
+  static const String heartFilled = 'assets/images/svgs/heart_filled.svg.vec';
+  static const String profile = 'assets/images/svgs/profile.svg.vec';
+  static const String profileFilled =
+      'assets/images/svgs/profile_filled.svg.vec';
+  static const String back = 'assets/images/svgs/back.svg.vec';
+  static const String setting = 'assets/images/svgs/setting.svg.vec';
+  static const String search = 'assets/images/svgs/search.svg.vec';
+  static const String star = 'assets/images/svgs/star.svg.vec';
+}
--- lib/features/home_screen/presentation/screens/home_screen.dart
+++ lib/features/home_screen/presentation/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce/core/utils/assets_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,99 +9,121 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 5,
-                    children: [
-                      Text(
-                        'Hello, Welcome 👋',
-                        style: TextStyle(fontSize: 11, color: Colors.grey),
-                      ),
-                      Text(
-                        'John Doe',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+    return Container(
+      color: theme.colorScheme.surface,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 5,
+                      children: [
+                        Text(
+                          'Hello, Welcome 👋',
+                          style: TextStyle(fontSize: 11, color: Colors.grey),
                         ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                    child: CircleAvatar(
-                      radius: 16,
-                      backgroundColor: theme.colorScheme.inverseSurface,
-                      child: Icon(
-                        Icons.person,
-                        color: theme.colorScheme.surface,
-                        size: 20,
-                      ),
+                        Text(
+                          'John Doe',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                spacing: 10,
-                children: [
-                  Flexible(
-                    flex: 5,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                        hint: Row(
-                          spacing: 5,
-                          children: [
-                            Icon(Icons.search, color: Colors.grey),
-                            Text(
-                              'Search...',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.zero,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                      child: CircleAvatar(
+                        radius: 16,
                         backgroundColor: theme.colorScheme.inverseSurface,
-                        foregroundColor: theme.colorScheme.surface,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        child: SvgPicture(
+                          AssetBytesLoader(AssetsManager.profile),
+                          colorFilter: ColorFilter.mode(
+                            theme.colorScheme.surface,
+                            BlendMode.srcIn,
+                          ),
+                          height: 20,
                         ),
-                        minimumSize: Size(55, 55),
                       ),
-                      child: Icon(Icons.settings, size: 26),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 10,
+                  children: [
+                    Flexible(
+                      flex: 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 14,
+                            horizontal: 14,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                          hint: Row(
+                            spacing: 10,
+                            children: [
+                              SvgPicture(
+                                AssetBytesLoader(AssetsManager.search),
+                                colorFilter: ColorFilter.mode(
+                                  Colors.grey,
+                                  BlendMode.srcIn,
+                                ),
+                              ),
+                              Text(
+                                'Search...',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          backgroundColor: theme.colorScheme.inverseSurface,
+                          foregroundColor: theme.colorScheme.surface,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minimumSize: Size(55, 53),
+                        ),
+                        child: SvgPicture(
+                          AssetBytesLoader(AssetsManager.setting),
+                          colorFilter: ColorFilter.mode(
+                            theme.colorScheme.surface,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            floating: true,
+            toolbarHeight: 140,
           ),
-          floating: true,
-          toolbarHeight: 140,
-        ),
-        SliverFillRemaining(),
-      ],
+          SliverFillRemaining(),
+        ],
+      ),
     );
   }
 }
