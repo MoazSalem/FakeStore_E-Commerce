0d6aa5bef2136ee771e17466b398f4efa1f7a688
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-08-25 12:54:18 +0300

    Initialize Project

---
+++ android/app/src/main/kotlin/com/moazsalem/ecommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.moazsalem.ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerce/features/home_screen/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
