1d5237ea8b124e3749f34ded2e68fa25e9296fe4
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-08-26 18:20:31 +0300

    Get products use case

---
+++ lib/features/home_screen/data/usecases/get_products_impl.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce/features/home_screen/domain/entities/product.dart';
+import 'package:ecommerce/features/home_screen/domain/repositories/product_repository.dart';
+import 'package:ecommerce/features/home_screen/domain/usecases/get_products.dart';
+
+class GetProductsImpl implements GetProducts {
+  final ProductRepository repository;
+  GetProductsImpl(this.repository);
+  Future<List<Product>> call() => repository.getProducts();
+}
