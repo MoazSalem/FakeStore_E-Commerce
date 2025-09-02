8caf7031aa877e67b9050393e86a30e0165efcb1
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-09-02 19:16:39 +0300

    Show correct price on cart screen

--- lib/features/cart_screen/presentation/screens/cart_screen.dart
+++ lib/features/cart_screen/presentation/screens/cart_screen.dart
@@ -17,80 +17,91 @@ class CartScreen extends StatelessWidget {
     return Column(
       children: [
         CustomAppBar(title: 'Cart'),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: SizesManager.padding20,
-          ),
-          child: SizedBox(
-            width: double.infinity,
-            height: 50,
-            child: OutlinedButton(
-              onPressed: () {},
-              child: Text(
-                "Checkout | \$695.00 ",
-                style: TextStyle(
-                  fontSize: SizesManager.font16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: SizesManager.padding20),
         BlocBuilder<CartCubit, CartState>(
           builder: (context, state) {
             if (state is CartLoaded) {
               return state.products.isNotEmpty
                   ? Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: SizesManager.padding10,
-                        ),
-                        child: ListView.builder(
-                          itemCount: state.products.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 10.0,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: SizesManager.padding20,
+                            ),
+                            child: SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: OutlinedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Checkout | \$${state.totalAmount} ",
+                                  style: TextStyle(
+                                    fontSize: SizesManager.font16,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  child: Stack(
-                                    alignment: Alignment.bottomRight,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: SizesManager.padding20),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: SizesManager.padding10,
+                              ),
+                              child: ListView.builder(
+                                itemCount: state.products.length,
+                                itemBuilder: (context, index) {
+                                  return Column(
                                     children: [
-                                      HorizontalItemWidget(
-                                        uniqueTag:
-                                            '01${state.products[index].id}',
-                                        product: state.products[index],
-                                      ),
                                       Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: AmountRow(
-                                          amount: state
-                                              .cart
-                                              .productsDetails[index]
-                                              .quantity,
-                                          onAdd: () {},
-                                          onRemove: () {},
-                                          amountTextSize: SizesManager.font14,
-                                          iconSize: SizesManager.iconSize14,
-                                          buttonsPadding: SizesManager.padding5,
-                                          spacing: SizesManager.padding10,
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 10.0,
+                                        ),
+                                        child: Stack(
+                                          alignment: Alignment.bottomRight,
+                                          children: [
+                                            HorizontalItemWidget(
+                                              uniqueTag:
+                                                  '01${state.products[index].id}',
+                                              product: state.products[index],
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(
+                                                8.0,
+                                              ),
+                                              child: AmountRow(
+                                                amount: state
+                                                    .cart
+                                                    .productsDetails[index]
+                                                    .quantity,
+                                                onAdd: () {},
+                                                onRemove: () {},
+                                                amountTextSize:
+                                                    SizesManager.font14,
+                                                iconSize:
+                                                    SizesManager.iconSize14,
+                                                buttonsPadding:
+                                                    SizesManager.padding5,
+                                                spacing: SizesManager.padding10,
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 10.0,
+                                        ),
+                                        child: CustomDivider(),
+                                      ),
                                     ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 10.0,
-                                  ),
-                                  child: CustomDivider(),
-                                ),
-                              ],
-                            );
-                          },
-                        ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     )
                   : const Expanded(child: Center(child: Text('No Products')));
