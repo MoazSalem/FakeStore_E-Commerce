6517f3db91afb8fce0132b6fb36222ad7fcbe9c6
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-09-24 12:50:44 +0300

    Initialize paymob service

--- lib/core/utils/consts_manager.dart
+++ lib/core/utils/consts_manager.dart
@@ -1,4 +1,8 @@
 class ConstsManager {
   static const String baseUrl = "https://fakestoreapi.com";
   static const String productsEndpoint = "/products";
+  static const String paymobApiKey = String.fromEnvironment('API_KEY');
+  static const String integrationID = String.fromEnvironment('CARD_ID');
+  static const String walletIntegrationId = String.fromEnvironment('WALLET_ID');
+  static const String iFrameID = String.fromEnvironment('IFRAME_ID');
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:ecommerce/app.dart';
 import 'package:ecommerce/core/dependency_injection/dependency_injection.dart';
+import 'package:ecommerce/core/utils/consts_manager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_paymob/flutter_paymob.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
+  await FlutterPaymob.instance.initialize(
+    apiKey: ConstsManager.paymobApiKey,
+    integrationID: int.parse(ConstsManager.integrationID),
+    walletIntegrationId: int.parse(ConstsManager.walletIntegrationId),
+    iFrameID: int.parse(ConstsManager.iFrameID),
+  );
   runApp(
     DevicePreview(
       defaultDevice: Devices.ios.iPhone16Pro,
